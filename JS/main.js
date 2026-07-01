cf453a76612b521f535c129aacfece68a998ba1f
Juan Cardenas <cardenasrivera1927@gmail.com>
2026-06-30 20:28:08 -0500

    Iniciando Repositorio

---
+++ JS/main.js
@@ -0,0 +1,37 @@
+const API_URL = "https://stock-flow-b661f-default-rtdb.firebaseio.com";
+const formLogin = document.getElementById("form-login");
+let usuarios = [];
+
+formLogin.addEventListener("submit",async (ev)=>{
+    ev.preventDefault();
+
+    const cc = document.getElementById("usuario");
+    const password = document.getElementById("password");
+
+    await obtenerUsuarios();
+
+    for(let i = 0; i< usuarios.length;i++){
+        if(usuarios[i].cc == cc.value && usuarios[i].password == password.value){
+            guardarSesion();
+            window.location.href = "usuarios.html"
+            return;
+        }
+    }
+    alert("Credenciales Incorrectas")
+});
+
+async function obtenerUsuarios(){
+    const response = await fetch(`${API_URL}/user/usuarios.json`,{
+        method:"GET",
+        headers:{
+            "Content-Type":"application/json"
+        }
+    });
+
+    usuarios = await response.json();
+
+}
+
+function guardarSesion(){
+
+}
